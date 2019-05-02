1697f0bc710cd276225f6ab5ace0b4cbfc5636a0
yoseas <50135810+yoseas@users.noreply.github.com>
2019-05-02 17:30:27 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class Main {
+    public static void main(String[] args){
+    Person student=new Person("Asal","Way");
+    System.out.println(student);
+    }
+}
