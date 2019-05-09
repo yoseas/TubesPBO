61b28744d8523b48264e2139954a78fd5b00d027
yoseas <yosesrgh@gmail.com>
2019-05-09 22:55:00 +0700

    Add

---
+++ Bank Soal ITERA/src/DesainUI/MyConnection.java
@@ -0,0 +1,29 @@
+package DesainUI;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author W I N D O W S  10
+ */
+public class MyConnection {
+    public static Connection getConnection(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/banksoal_itera", "root","");
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        return con;
+    }
+
+}
